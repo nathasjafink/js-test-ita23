fe60c2951e3e3256ff69f594b2c045cd2015860d
nathasjafink <nathasja.nielsen@gmail.com>
2023-10-13 09:24:25 +0200

    02 done

--- src/02-loops-conditional.js
+++ src/02-loops-conditional.js
@@ -10,8 +10,17 @@
  */
 
 function isEven(number) {
-    
+    const n = number;
+    if (n % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+const total =isEven(4);
+console.log(total);
+const anothertotal = isEven(7);
+console.log(anothertotal);
 
 /**
  * This function checks if a number is within a range.
@@ -27,8 +36,12 @@ function isEven(number) {
  */
 
 function isWithinRange(n, min, max) {
-
+    const number = n;
+    return ((number-min)*(number-max) <= 0);
 }
+const inRange = isWithinRange(5,1,10);
+console.log(inRange);
+const outOfRange = isWithinRange(15,1,10);
 
 /**
  * This function checks if a string contains the string "ba"
@@ -43,8 +56,14 @@ function isWithinRange(n, min, max) {
  */
 
 function stringContainsBa(string) {
-
+    const checkString = string.includes("ba");
+    return checkString;
 }
+const total1 = stringContainsBa("Banana");
+console.log(total1);
+const total2 = stringContainsBa("harbor");
+console.log(total2);
+
 
 
 
